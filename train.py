d3ad526e23b5789b99f9fb551cbcbf03574a03e5
jwngo <ngojw7@gmail.com>
2020-06-01 15:59:57 +0800

    changed dataloader, removed sampler and used DataLoader

--- train.py
+++ train.py
@@ -42,35 +42,23 @@ class Trainer(object):
                 image_set = 'val',
                 transforms = input_transform,
                 )
-        self.iters_per_epoch = len(self.train_dataset) // cfg['TRAIN']['BATCH_SIZE']
-        self.max_iters = cfg['TRAIN']['MAX_EPOCHS'] * self.iters_per_epoch
-
-        self.train_sampler = data.sampler.RandomSampler(self.train_dataset) 
-        self.val_sampler = data.sampler.RandomSampler(self.val_dataset) 
-        
-        self.train_batch_sampler = data.sampler.BatchSampler(
-            self.train_sampler, 
-            cfg['TRAIN']['BATCH_SIZE'],
-            drop_last=True,
-         )
-        self.val_batch_sampler = data.sampler.BatchSampler(
-            self.val_sampler,
-            cfg['TRAIN']['BATCH_SIZE'],
-            drop_last=False
-        )
-
+                
         self.train_loader = data.DataLoader(
-            dataset=self.train_dataset,
-            batch_sampler=self.train_batch_sampler,
-            num_workers=0,
-            pin_memory=True
-        )
+                dataset = self.train_dataset,
+                batch_size = 16,
+                shuffle = True,
+                num_workers = 0,
+                pin_memory = True,
+                drop_last = True,
+                )
         self.val_loader = data.DataLoader(
-            dataset=self.val_dataset,
-            batch_sampler=self.val_batch_sampler,
-            num_workers=0,
-            pin_memory=True
-        )
+                dataset = self.val_dataset,
+                batch_size = 16, 
+                shuffle = True,
+                num_workers = 0, 
+                pin_memory = True,
+                drop_last = False,
+                ) 
 
         # -------- network --------
         self.model = ENet(num_classes=5).to(self.device) 
